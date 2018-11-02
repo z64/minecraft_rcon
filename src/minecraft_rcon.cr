d7c72d77af5d6dd44a027126e389564dd5ce0794
Zac Nowicki <zachnowicki@gmail.com>
2018-11-02 17:10:13 +0000

    Add commands

--- src/minecraft_rcon.cr
+++ src/minecraft_rcon.cr
@@ -121,5 +121,80 @@ module Minecraft::RCON
       packet = Packet.new(next_request_id, :command, command)
       send(packet)
     end
+
+    # :ditto:
+    def execute(command : String, *args)
+      string = args.to_a.unshift(command).join(' ')
+      execute(string)
+    end
+
+    # Revokes operator status from the respective player
+    def deop(player : String)
+      execute("deop", player)
+    end
+
+    # Kicks the respective player
+    def kick(player : String, reason : String? = nil)
+      execute("kick", player, reason)
+    end
+
+    # Makes the respective player an operator
+    def mkop(player : String)
+      execute("op", player)
+    end
+
+    # Teleports one player to the other player
+    def teleport(player : String, destination_player : String)
+      execute("tp", player, destination_player)
+    end
+
+    # Teleports one player to a specific position
+    def teleport(player : String, location : Tuple(Float64, Float64, Float64))
+      execute("tp", *location)
+    end
+
+    # Teleports one player to a specific position, with orientation
+    def teleport(player : String, location : Tuple(Float64, Float64, Float64),
+                 orientation : Tuple(Float64, Float64))
+      x, y, z = location
+      pitch, yar = orientation
+      execute("tp", x, y, z, pitch, yaw)
+    end
+
+    # Sends a message from RCON in first-person perspective
+    def me(message : String)
+      execute("me", message)
+    end
+
+    # Broadcast a message to all players
+    def say(message : String)
+      execute("say", message)
+    end
+
+    # Sends a URL to the specified player
+    def send_url(player : String, url : String, text : String? = nil)
+      payload = {text: text, clickEvent: {action: "open_url", value: url}}
+      tell_raw(player, payload)
+    end
+
+    # Sends a message represented by a JSON object
+    def tell_raw(player : String, object)
+      execute("tellraw", player, object.to_json)
+    end
+
+    # Returns the players on the server
+    def players
+      execute("list")
+    end
+
+    # Returns the server seed
+    def seed
+      execute("seed")
+    end
+
+    # Locates the respective structure
+    def locate(structure : String)
+      execute("locate", structure)
+    end
   end
 end
